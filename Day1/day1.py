d8b151968d182ad8254311dcf5e1ed41fd098e88
darpan45 <44457866+darpan45@users.noreply.github.com>
2021-04-07 17:37:45 +0530

    Add files via upload
    Day 1 assignment

---
+++ Day1/day1.py
@@ -0,0 +1,30 @@
+# #Print Statement
+# #1.1
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# print("hello World\nHello World")
+
+# # 1.2
+# #String Concatination
+# print("Hello" + " "+ "Darpan")
+
+# #1.3
+# #input function
+# print(len(input("What is your Name ? ")))
+
+# 1.4
+# a=input("a : ")
+# b=input("b : ")
+# a,b=b,a
+# print("a = " + a)
+# print("b = " + b)
+
+# Band Name Generator
+print("Welcome to the Band Name Generator")
+cityName=input("What's name of the city you grew up in ?\n")
+petName=input("What's your pet's name ?\n")
+print("Your band name could be " +cityName +" "+petName+ "." )
+
+
